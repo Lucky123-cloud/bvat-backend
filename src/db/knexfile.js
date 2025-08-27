ab91d250db8362d2d32cd92782b5d1c538283981
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-08-27 13:35:36 +0300

    added the test db

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,18 +1,23 @@
+require('dotenv').config();
+const path = require('path');
+
 const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME,
-    user: DB_USER,
-    password: DB_PASSWORD,
-  },
-  migrations: {
-    directory: './db/migrations',
-  },
-  seeds: {
-    directory: './db/seeds',
+  development: {
+    client: 'pg',
+    connection: {
+      host: DB_HOST,
+      port: DB_PORT,
+      database: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
+    },
+    migrations: {
+      directory: path.join(__dirname, 'migrations'), // Correct path
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds'), // Correct path
+    },
   },
 };
